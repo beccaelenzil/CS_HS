84871687b99b836ece23358af057fa353095106e
hirosme <hirosme@gmail.com>
2016-10-10 12:47:24 -0700

    Commit with 21 game

--- 21.py
+++ 21.py
@@ -1,24 +1,38 @@
 import random
+import time
 counter = 21
 def play():
-    playfirst = raw_input('Would you like to go first? (yes or no)')
-    if playfirst == 'yes':
-        yourturn()
-    elif playfirst == 'no':
-        myturn()
-    else:
-        print 'Please choose yes or no'
+    valid = 0
+    while valid == 0:
+        try:
+            playfirst = raw_input('Would you like to go first? (yes or no)')
+            if playfirst == 'yes':
+                valid = 1
+                yourturn()
+            elif playfirst == 'no':
+                valid = 1
+                myturn()
+            else:
+                print 'Please choose yes or no'
+        except:
+            print 'Please choose yes or no'
 
 def yourturn():
     global counter
     global turn
     userchoice = 0
     print 'Counter is:', counter
+    time.sleep(1)
     while userchoice not in range(1,4):
-        userchoice = int(raw_input('By how much would you like to subtract? (1-3)'))
+        try:
+            userchoice = int(raw_input('By how much would you like to subtract? (1-3)'))
+        except:
+            userchoice = 4
     counter = counter - userchoice
+    time.sleep(1)
     if counter == 0:
         print 'Counter is', counter
+        time.sleep(1)
         print 'You win'
     else:
         myturn()
@@ -29,10 +43,13 @@ def myturn():
         computerchoice = random.randint(1,3)
     else:
         computerchoice = counter%4
+    time.sleep(1)
     print 'Computer chooses:', computerchoice
     counter = counter - computerchoice
     if counter == 0:
+        time.sleep(1)
         print 'Counter is:', counter
+        time.sleep(1)
         print 'I win'
     else:
         yourturn()
