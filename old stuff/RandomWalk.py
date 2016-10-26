914dd4bb3e0936570cb244ec73958b0e72ea2340
hirosme <hirosme@gmail.com>
2016-10-25 22:30:39 -0700

    Added analysis in randomwalk

--- old stuff/RandomWalk.py
+++ old stuff/RandomWalk.py
@@ -1,5 +1,6 @@
 import random
 import time
+import math
 def rs():
     return random.choice([-1,1])
 def rwPos(start,nsteps):
@@ -36,30 +37,47 @@ def rwPosPlain(start,nsteps):
         currentPosition = currentPosition + rs()
     return currentPosition
 
-def ave_signed_displacement(numtrials):
+def ave_signed_displacement(numtrials,steps):
     '''return average displacement of particle taking 100
     steps in numtrials trials'''
     counter = 0
     for i in range(numtrials):
-        counter += rwPosPlain(0,100)
+        a = rwPosPlain(0,steps)
+        counter += math.sqrt(a**2)
     return counter/numtrials
-def ave_squared_displacement(numtrials):
-    '''return average squared displacement of particle taking 100
+def ave_squared_displacement(numtrials,steps):
+    '''return average squared displacement of particle taking steps
     steps in numtrials trials'''
     counter = 0
     for i in range(numtrials):
-        counter += rwPosPlain(0,100)**2
+        counter += float(rwPosPlain(0,steps)**2)
     return counter/numtrials
-print ave_signed_displacement(100)
-print ave_squared_displacement(100)
-
+#print ave_signed_displacement(1000,200)
+#print ave_squared_displacement(1000,200)
+def avesignedave(numtrials):
+    '''return a list of the average signed distance of every step value for the first numtrials
+    multiples of 5'''
+    list = []
+    for i in range(numtrials):
+        list.append([5*i,ave_signed_displacement(500,5*i)])
+    return list
+print avesignedave(30)
 """
    In order to compute the average signed displacement for
    a random walker after 100 random steps, I ran rwPosPlain
    a number of times, adding the total to a counter, and then
    dividing the counter by the number of trials at the end. I
    did the same thing with the average squared displacement.
-
-    ave_signed_displacement: 1, -1, -1
-    ave_squared_displacement: 87,112,115
+    100 trials:
+    ave_signed_displacement: 7.62, 9.2, 7.86
+    ave_squared_displacement: 96.96,103.92,105.0
+    After running 100 trials 1000 times:
+    signed: 7.61
+    squared: 96.316
+    50 trials 1000 times:
+    signed: 5.594
+    squared 47.74
+    200 trials 1000 times:
+    signed: 10.85
+    squared: 191.028
 """
